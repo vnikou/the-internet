f639762836b55e2b87bf1b4892febaa37fe99b69
Vasiliki Nikou <v.nikou@gmail.com>
2020-09-21 09:09:45 +0300

    Assertions for checkboxes

--- pages/tests.py
+++ pages/tests.py
@@ -1,13 +1,20 @@
 from utilities.startup import open_site
 from config import base_url
 browser = open_site(base_url)
-
+assert "The Internet" in browser.title
 # checkboxes page
 checkbox = browser.find_element_by_xpath('//a[text()="Checkboxes"]').click()
 
-choice = browser.find_element_by_xpath('//input[@type="checkbox"][2]')
+checkbox2 = browser.find_element_by_xpath('//input[@type="checkbox"][2]')
 
-if choice.is_selected() == False:
-    choice.click()
+if checkbox2.is_selected() == False:
+    checkbox2.click()
+
+checkbox1 = browser.find_element_by_xpath('//input[@type="checkbox"][1]')
+
+if checkbox1.is_selected() == False:
+    checkbox1.click()
+
+assert checkbox1.get_attribute('checked')
 
 browser.back()
