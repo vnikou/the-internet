01b8ca7e37afb4fb1ff6d35ccd566ee32a4dd2ae
Vasiliki Nikou <v.nikou@gmail.com>
2020-10-13 22:53:19 +0300

    Alert window

--- pages/tests.py
+++ pages/tests.py
@@ -1,13 +1,50 @@
 from utilities.startup import open_site
 from config import base_url
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.alert import Alert
+from selenium.webdriver import ActionChains
+
+import time
 
 browser = open_site(base_url)
 
 assert "The Internet" in browser.title
 
+con = browser.find_element_by_xpath('//a[text()="Context Menu"]').click()
+source = browser.find_element_by_id("hot-spot")
+action = ActionChains(browser)
+action.context_click(source).perform()
+obj = browser.switch_to.alert
+obj.accept()
+
+
+button = browser.find_element_by_name("alert").click()
+#Switch the control to the Alert window
+
+print(" Clicked on the OK Button in the Alert Window")
+
+browser.back()
+
+
+img = browser.find_element_by_xpath('//a[text()="Broken Images"]').click()
+images = browser.find_elements_by_tag_name('img')
+images.__getattribute__('src')
+
+if images.getattribute("src"):
+    print("dsdsd")
+assert isinstance(images.getattribute, 'asdf.jpg')
+
+
+
+browser.back()
+
+authedication = browser.find_element_by_xpath('//a[text()="Basic Auth"]').click()
+alert  = browser.switch_to_alert()
+alert.send_keys('admin')
 
 # checkboxes page
 checkbox = browser.find_element_by_xpath('//a[text()="Checkboxes"]').click()
+
 
 checkbox2 = browser.find_element_by_xpath('//input[@type="checkbox"][2]')
 
@@ -37,4 +74,19 @@ inputs2 = browser.find_element_by_xpath('//input')
 inputs2.send_keys('123456789')
 
 assert inputs2.get_attribute('value') == '123456789'
+
+browser.back()
+
+checkbox = browser.find_element_by_xpath('//a[text()="Add/Remove Elements"]').click()
+add = browser.find_element_by_xpath('//button[text()="Add Element"]').click()
+delete = browser.find_element_by_xpath('//button[text()="Delete"]').click()
+browser.back()
+
+
+
+
+
+
+
+
 
--- utilities/startup.py
+++ utilities/startup.py
@@ -2,12 +2,12 @@ def open_site(base_url):
 
 
     from selenium import webdriver
-    from selenium.webdriver.common.keys import Keys
+    from webdriver_manager.chrome import ChromeDriverManager
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+
+    from selenium.webdriver.common.alert import Alert
 
     browser = webdriver.Chrome('C:\\webdrivers\\chromedriver')
     browser.get(base_url)
     browser.maximize_window()
     return browser
-
- 
-
